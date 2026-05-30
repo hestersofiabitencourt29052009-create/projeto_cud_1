0584b575bed0708bba1d0aa5da75a2100f1fe3a6
hestersofiabitencourt29052009-create <hestersofiabitencourt29052009@gmail.com>
2026-05-30 12:49:38 -0300

    Update model.py

--- model.py
+++ model.py
@@ -1,14 +1,14 @@
-# banco de dados - modelagem 
+# banco de dados -  modelagem  
 
-import sqlite3 
+import sqlite3
+
 def salvar_no_banco(texto):
     conn = sqlite3.connect('dados.db')
-    c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS notas (texo TEXT)')
-    c.execut('INSERT INTO notas VALUES(?) ', (texto))
+    c =  conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS notas (texto TEXT)")
+    c.execute("INSERT INTO notas VALUES(?)",(texto,))
     conn.commit()
     conn.close()
-
 
 def ler_do_banco():
     conn = sqlite3.connect('dados.db')
