ee3d3a323611bdf37da575b9e3b6416667081766
hestersofiabitencourt29052009-create <hestersofiabitencourt29052009@gmail.com>
2026-05-30 12:39:46 -0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,11 +1,12 @@
 # controlar e conectar 
 
-import model 
-def adicionar_nota(texto):
-    if texto.trip9:
-        model.salvar_no_banco(texto)
-        return True 
-    return false 
+import model
 
-    def listar_notas():
-        return model.ler_do_banco()    
+def adicionar_nota(texto):
+    if texto.strip():
+        model.salvar_no_banco(texto)
+        return True
+    return False
+
+def listar_notas():
+    return model.ler_do_banco()    
